fd480e425fcc1c09c52a7b2d60877f317aa61abc
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-21 11:05:05 +0100

    2nd commit main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import React from 'react';
+import About from "./components/About";
+import Home from "./components/Home";
+import Pricing from "./components/Pricing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 const App = ()=> {
   
   return (
-    <h1>Edit Me</h1>
+
   )
 
 }
